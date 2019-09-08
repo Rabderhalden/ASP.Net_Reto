320d0ad8e95b1799d46a10ef7bd4a2705a2dbcf5
R_Abderhalden <reto88@gmx.net>
2019-09-08 14:30:30 +0200

    Registrierung ohne Liste, ohne Strongly-Typed-View, ohne DB

---
+++ Aufgabe1_Registrierung/Controllers/SpielerDetailController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aufgabe1_Registrierung.Controllers
+{
+    public class SpielerDetailController : Controller
+    {
+        public IActionResult SeeSpieler(string Vorname, string Nachname, string Telefon, string Email, string Jahrgang)
+        {
+            ViewBag.Vorname = Vorname;
+            ViewBag.Nachname = Nachname;
+            ViewBag.Telefon = Telefon;
+            ViewBag.Email = Email;
+            ViewBag.Jahrgang = Jahrgang;
+            return View();
+        }
+    }
+}
---
+++ Aufgabe1_Registrierung/Controllers/SpielerListeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aufgabe1_Registrierung.Controllers
+{
+    public class SpielerListeController : Controller
+    {
+        public IActionResult SeeList()
+        {
+            return View();
+        }
+    }
+}
