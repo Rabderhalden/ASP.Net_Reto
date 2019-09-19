43eeb15d7dead9d8fa0297c788fe63f7dc420acc
R_Abderhalden <reto88@gmx.net>
2019-09-19 21:01:22 +0200

    Bower (Bootstrap) hinzugefügt. > erst navi Design

--- Aufgabe1_Registrierung/Startup.cs
+++ Aufgabe1_Registrierung/Startup.cs
@@ -21,6 +21,8 @@ namespace Aufgabe1_Registrierung
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseStaticFiles();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
